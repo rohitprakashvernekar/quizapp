5ea7ab80ac1581665684fd93f9088364a4928e4d
rohitvernekar7 <rohitvernekar7@yahoo.com>
2020-12-23 21:48:31 +0530

    implemented select answer , result component which displays result and allows to playAgain

--- src/components/QuestionBox.js
+++ src/components/QuestionBox.js
@@ -1,9 +1,20 @@
-import React from 'react-dom';
+import React, { useState } from 'react';
 
-const QuestionBox = ({question, options}) => {
+const QuestionBox = ({question, options, selected}) => {
+    const [answer, setAnswer ] = useState(options);
     return (
-        <div className="questionBox"> {question}</div>
+        <div className="questionBox"> 
+            <div className="question">{question}</div>
+            {answer.map((text, index) =>
+            <button key={index} className="answerBtn" onClick={() => {
+                setAnswer([text]);
+                selected(text);
+            }}>
+                {text}
+            </button>
+            )}
+        </div>
     );
-}
+};
 
 export default QuestionBox;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDom from 'react-dom';
 import './assets/style.css';
 import quizService from './quizService';
 import QuestionBox from './components/QuestionBox';
+import Result from './components/Result';
 
 class QuizApp extends Component {
     state = {
-        questionBank: []
+        questionBank: [],
+        score: 0,
+        responses: 0
     };
+
     getQuestions = () => {
         quizService().then(question => {
             this.setState({
@@ -15,6 +19,26 @@ class QuizApp extends Component {
             });
         });
     };
+
+    computeAnswer =( answer, correctAnswer) => {
+        if (answer === correctAnswer){
+            this.setState({
+                score: this.state.score + 1
+            });
+        }
+        this.setState({
+            responses: this.state.responses < 5 ? this.state.responses + 1 : 5
+        });
+    }
+
+    playAgain = () => {
+        this.getQuestions();
+        this.setState({
+            score: 0,
+            responses: 0
+        });
+    }
+
     componentDidMount() {
         this.getQuestions();
     }
@@ -23,10 +47,21 @@ class QuizApp extends Component {
             <div className='container'>
                 <div className='title'>QuizApp</div>
                 {this.state.questionBank.length > 0 && 
+                this.state.responses < 5 &&
                 this.state.questionBank.map(
-                    ({question, answers, correct, questionId}) => 
-                    <QuestionBox question={question} options={answers} key={questionId}/>
+                    ({question, answers, correct, questionId}) => (
+                    <QuestionBox 
+                    question={question} 
+                    options={answers} 
+                    key={questionId}
+                    selected={ answer => this.computeAnswer(answer, correct)}    
+                    />
+                )
                 )}
+                
+                {this.state.responses === 5 ? ( 
+                    <Result score ={this.state.score} playAgain={this.playAgain} /> 
+                    ) : null } 
             </div>
         );
     }
